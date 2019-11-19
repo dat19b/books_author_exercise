bfe9f78cb4f977d98e551c0fb959d42a2086a82d
Liam Blake <liamblakedk@hotmail.com>
2019-11-19 08:22:56 +0100

    start

---
+++ src/main/java/kea/clbo/model/Author.java
@@ -0,0 +1,22 @@
+package kea.clbo.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books  = new HashSet<>();
+
+
+
+}
