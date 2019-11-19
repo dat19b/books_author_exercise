cb9ab686c776c30b35cddb42af7087dd60f9bbcc
Liam Blake <liamblakedk@hotmail.com>
2019-11-19 08:32:33 +0100

    master

--- src/main/java/kea/clbo/model/Author.java
+++ src/main/java/kea/clbo/model/Author.java
@@ -1,22 +1,13 @@
 package kea.clbo.model;
 
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity
-public class Author {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class Author {
 
     private String firstName;
     private String lastName;
 
-    @ManyToMany(mappedBy = "authors")
     private Set<Book> books  = new HashSet<>();
-
-
-
 }
--- src/main/java/kea/clbo/model/Book.java
+++ src/main/java/kea/clbo/model/Book.java
@@ -1,23 +1,15 @@
 package kea.clbo.model;
 
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity
-public class Book {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class Book {
 
     private String title;
     private String isbn;
     private String publisher;
 
-    @ManyToMany
-    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name= "book_id"),
-            inverseJoinColumns = @JoinColumn(name="author_id"))
     private Set<Author> authors = new HashSet<>();
 
 }
